c1c3fbe616d4e06917e606006104f599e1d4246a
Obedullah Ansari <154742900+Obedullah-ansari@users.noreply.github.com>
2024-11-10 17:37:56 +0530

    Update update-data.js

--- devData/update-data.js
+++ devData/update-data.js
@@ -42,7 +42,7 @@ const updateDataBase = async (newsType) => {
       const response = await request({
         uri: sources.url,
         headers: sources.headers,
-        gzip: false,
+        gzip: true,
       });
 
       const $ = cheerio.load(response);
